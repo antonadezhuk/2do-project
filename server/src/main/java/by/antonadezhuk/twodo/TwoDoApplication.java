85303f78ba4f62d07dc9f12e9e7dcfd9ce4ed068
antonadezhuk <anton.nadezhuk@gmail.com>
2024-03-29 16:57:14 +0300

    Initial commit

---
+++ server/src/main/java/by/antonadezhuk/twodo/TwoDoApplication.java
@@ -0,0 +1,13 @@
+package by.antonadezhuk.twodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoDoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/by/antonadezhuk/twodo/TwoDoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.antonadezhuk.twodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
